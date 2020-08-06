711d578121d5adcd06a5a3fb4cfb52463640648c
vincenttuan <vincenttuan@gmail.com>
2020-08-06 19:57:24 +0800

    Python

--- atm/ATM.py
+++ atm/ATM.py
@@ -12,6 +12,26 @@ def display():
         for key in act.keys():
             print(key, act.get(key))
 
+def save():
+    actName = input('請輸入存款人')
+    x = int(input('請輸入存款金額'))
+    # 得到存款人的account物件
+    account = None
+    for act in list:
+        for key in act.keys():
+            if key == actName:
+                account = act.get(key)
+    if account == None:
+        print('查無此人')
+    else:
+        account.save(x)
+
+def withdraw():
+    pass
+
+def transfer():
+    pass
+
 # 系統選單
 while True:
     print('系統選單:')
@@ -26,11 +46,11 @@ while True:
     if no == 1:
         display()
     elif no == 2:
-        pass
+        save()
     elif no == 3:
-        pass
+        withdraw()
     elif no == 4:
-        pass
+        transfer()
     elif no == 5:
         break
     os.system('pause') # 暫停(按下任意見後繼續...)
--- atm/Account.py
+++ atm/Account.py
@@ -1,7 +1,11 @@
 class Account:
     __money = 0
+
     def __init__(self, x) -> None:
         self.__money = x
+
+    def save(self, x):
+        self.__money -= x
 
     def __str__(self) -> str:
         return "帳戶餘額: $" + str(self.__money)
