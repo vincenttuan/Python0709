f6f45a28dad96efac6e98a3ee61057e9f8eafcd4
vincenttuan <vincenttuan@gmail.com>
2020-08-06 19:29:46 +0800

    Python

---
+++ atm/ATM.py
@@ -0,0 +1,7 @@
+import atm.Account as act
+# 主程式
+act1 = act.Account(1000)
+act2 = act.Account(1000)
+act3 = act.Account(1000)
+list = [{"john": act1}, {"mary": act2}, {"tom": act3}]
+
---
+++ atm/Account.py
@@ -0,0 +1,5 @@
+class Account:
+    __money = 0
+    def __init__(self, x) -> None:
+        self.__money = x
+
