fd9677dd1a59124dd7419bff22e8c7c6b366bea8
vincenttuan <vincenttuan@gmail.com>
2020-08-06 19:43:53 +0800

    Python

--- atm/ATM.py
+++ atm/ATM.py
@@ -5,3 +5,32 @@ act2 = act.Account(1000)
 act3 = act.Account(1000)
 list = [{"john": act1}, {"mary": act2}, {"tom": act3}]
 
+# 列出所有人的帳戶餘額
+def display():
+    for act in list:
+        for key in act.keys():
+            print(key, act.get(key))
+
+# 系統選單
+while True:
+    print('系統選單:')
+    print('----------')
+    print('1. 查詢')
+    print('2. 存款')
+    print('3. 提款')
+    print('4. 轉帳')
+    print('5. 離開')
+    print('----------')
+    no = int(input('請選擇(1~5): '))
+    if no == 1:
+        display()
+    elif no == 2:
+        pass
+    elif no == 3:
+        pass
+    elif no == 4:
+        pass
+    elif no == 5:
+        break
+
+print('程式結束')
--- atm/Account.py
+++ atm/Account.py
@@ -3,3 +3,6 @@ class Account:
     def __init__(self, x) -> None:
         self.__money = x
 
+    def __str__(self) -> str:
+        return "帳戶餘額: $" + str(self.__money)
+
