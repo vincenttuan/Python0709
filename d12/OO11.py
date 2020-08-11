d66f5fcdeb0b8223ddc419e2cf1720c8cde90781
vincenttuan <vincenttuan@gmail.com>
2020-08-11 21:26:25 +0800

    Python

--- d12/OO11.py
+++ d12/OO11.py
@@ -5,12 +5,12 @@ class Celsius: # 攝氏
         instance.fahrenheit = 32 + 9 * value / 5
 
 class Temperature: # 溫度
-    celsius = Celsius()
+    celsius = Celsius()  # 內部類別
     def __init__(self, initial_f):
         self.fahrenheit = initial_f # fahrenheit = 華氏
 
 if __name__ == '__main__':
     t = Temperature(212)
-    print(t.celsius)
-    t.celsius = 0
+    print(t.celsius) # 呼叫 __get__
+    t.celsius = 0    # 呼叫 __set__
     print(t.fahrenheit)
