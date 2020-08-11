322c965c1314724c4152b40e7edb968dd491c140
vincenttuan <vincenttuan@gmail.com>
2020-08-11 21:15:15 +0800

    Python

--- d12/OO10.py
+++ d12/OO10.py
@@ -7,9 +7,15 @@ class Drink:
     def __lt__(self, other):
         return (self.price/self.amount) < (other.price/other.amount)
 
+    def __gt__(self, other):
+        return (self.price / self.amount) > (other.price / other.amount)
+
+
 if __name__ == '__main__':
     milk   = Drink('牛奶', 2, 80)
     coffee = Drink('咖啡', 3, 110)
     print(coffee < milk)
     drinkName = coffee.name if coffee < milk else milk.name
-    print(drinkName)
+    print("便宜的是:", drinkName)
+    drinkName = coffee.name if coffee > milk else milk.name
+    print("貴的是:", drinkName)
