8d235d45df78b509a144e302987e7d0e0dbd7864
vincenttuan <vincenttuan@gmail.com>
2020-08-13 21:11:48 +0800

    Python

--- d14/GUI4.py
+++ d14/GUI4.py
@@ -1,24 +1,23 @@
 # 現在時間
 import tkinter
 import datetime
-import time
 
-def play():
+def update():
     dt = datetime.datetime.today()
     ans.set(dt)
-
 
 win = tkinter.Tk()
 win.title("My Add")
 win.geometry("500x100")
 
 ans = tkinter.StringVar()
-ans.set("")
+update()
+
 label = tkinter.Label(win, textvariable=ans)
 label.config(font=("Arial", 20))
 label.pack()
 
-addButton = tkinter.Button(win, text="Start", command=play)
+addButton = tkinter.Button(win, text="Update", command=update)
 addButton.config(font=("Arial", 20))
 addButton.pack()
 
