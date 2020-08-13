971625ace8601d82816a6a8b4fcda6648c0b7f87
vincenttuan <vincenttuan@gmail.com>
2020-08-13 20:47:37 +0800

    Python

--- d14/GUI_LabelAndButton.py
+++ d14/GUI_LabelAndButton.py
@@ -1,4 +1,11 @@
 import tkinter
+from tkinter import messagebox
+
+def hello():
+    messagebox.showinfo('Hello', 'Python')
+
+def cancel():
+    win.quit()
 
 win = tkinter.Tk()
 
@@ -8,11 +15,11 @@ label = tkinter.Label(win, text="Hello")
 label.config(font=("Arial", 40), bg="Red", fg="Yellow")
 label.pack()
 
-button1 = tkinter.Button(win, text="OK")
+button1 = tkinter.Button(win, text="OK", command=hello)
 button1.config(font=("Arial", 30))
 button1.pack(side=tkinter.LEFT)
 
-button2 = tkinter.Button(win, text="Exit")
+button2 = tkinter.Button(win, text="Exit", command=cancel)
 button2.config(font=("Arial", 30))
 button2.pack(side=tkinter.RIGHT)
 
