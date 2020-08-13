26e413cd462106331a02d048c7abcb1bf82d21e4
vincenttuan <vincenttuan@gmail.com>
2020-08-13 19:19:09 +0800

    Python

--- d13/Login.py
+++ d13/Login.py
@@ -5,14 +5,32 @@ users = [
 
 def loginCheck(u, p):
     check = None
+    # 先確認是否有找帳號 ?
+    for user in users:
+        if user['name'] == u:
+            check = True
+
+    if not check:
+        e = Exception('無此帳號')
+        raise e # 將錯誤物件拋出
+
+    # 密碼是否正確 ?
+    check = None
     for user in users:
         if user['name'] == u and user['password'] == p:
             check = True
-        else:
-            check = False
+
+    if not check:
+        e = Exception('密碼錯誤')
+        raise e # 將錯誤物件拋出
+
     return check
 
 if __name__ == '__main__':
-    result = loginCheck('abc', '1111')
-    print(result)
+    try:
+        result = loginCheck('admin', '1234')
+    except Exception as e:
+        print(e)
+    else:
+        print(result)
 
