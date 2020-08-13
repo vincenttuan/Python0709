5ebd16133c617b1a71c9e85e05d9ff3e0d57adf9
vincenttuan <vincenttuan@gmail.com>
2020-08-13 18:53:58 +0800

    Python

--- d13/InputNumber2.py
+++ d13/InputNumber2.py
@@ -3,7 +3,6 @@ def inputNumber():
         x = int(input('請輸入分子:'))
         y = int(input('請輸入分母:'))
         z = x / y
-        print(x, '/', y, '=', z)
     except ValueError as e:
         print('錯誤類型:', e.__class__.__name__)
         print('您輸入的不是數字, 請重新輸入')
@@ -16,6 +15,8 @@ def inputNumber():
         print('錯誤類型:', e.__class__.__name__)
         print('其他錯誤, 程式結束...')
         return
+    else:
+        print(x, '/', y, '=', z)
 
 if __name__ == '__main__':
     inputNumber()
