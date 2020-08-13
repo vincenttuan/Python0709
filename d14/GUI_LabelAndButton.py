058b3fc72897c504c6aa7ff29dff8a9266da3f0b
vincenttuan <vincenttuan@gmail.com>
2020-08-13 20:44:01 +0800

    Python

--- d14/GUI_LabelAndButton.py
+++ d14/GUI_LabelAndButton.py
@@ -7,9 +7,13 @@ win.geometry("300x300")
 label = tkinter.Label(win, text="Hello")
 label.config(font=("Arial", 40), bg="Red", fg="Yellow")
 label.pack()
+
 button1 = tkinter.Button(win, text="OK")
+button1.config(font=("Arial", 30))
 button1.pack(side=tkinter.LEFT)
+
 button2 = tkinter.Button(win, text="Exit")
+button2.config(font=("Arial", 30))
 button2.pack(side=tkinter.RIGHT)
 
 win.mainloop()
