c11da20137e40d925e49629b96ee89f9f70a5583
vincenttuan <vincenttuan@gmail.com>
2020-08-15 09:43:10 +0800

    Python

---
+++ opencv/OpenCVTest.py
@@ -0,0 +1,12 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+while True:
+    ret, frame = cap.read()
+    print(ret, frame)
+
+cap.release()
+cv2.destroyAllWindows()
