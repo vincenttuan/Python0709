722f38cb6ce1db4beba1a81134268f2e577ec2ed
vincenttuan <vincenttuan@gmail.com>
2020-08-18 21:53:17 +0800

    Python

--- firebase/Demo3.py
+++ firebase/Demo3.py
@@ -16,7 +16,7 @@ openButton = tkinter.Button(win, text="Open", command=openTheDoor)
 openButton.config(font=("Arial", 20))
 openButton.pack(side=tkinter.LEFT)
 
-closeButton = tkinter.Button(win, text="Open", command=closeTheDoor)
+closeButton = tkinter.Button(win, text="Close", command=closeTheDoor)
 closeButton.config(font=("Arial", 20))
 closeButton.pack(side=tkinter.RIGHT)
 
