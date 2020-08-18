37299c40b82ffcfee74e0d30d33ac8f00d1915a2
vincenttuan <vincenttuan@gmail.com>
2020-08-18 21:49:32 +0800

    Python

---
+++ firebase/Demo3.py
@@ -0,0 +1,21 @@
+import tkinter
+
+def openTheDoor():
+    pass
+
+def closeTheDoor():
+    pass
+
+win = tkinter.Tk()
+win.title("Door Open")
+win.geometry("300x100")
+
+openButton = tkinter.Button(win, text="Open", command=openTheDoor)
+openButton.config(font=("Arial", 20))
+openButton.pack(tkinter.LEFT)
+
+closeButton = tkinter.Button(win, text="Open", command=closeTheDoor)
+closeButton.config(font=("Arial", 20))
+closeButton.pack(tkinter.RIGHT)
+
+win.mainloop()
