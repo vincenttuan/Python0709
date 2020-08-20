7c504200820bc7dba6732427fe712d805bcfc8a4
vincenttuan <vincenttuan@gmail.com>
2020-08-20 19:29:07 +0800

    Python

---
+++ d15/CtoF.py
@@ -0,0 +1,36 @@
+import tkinter
+import tkinter.ttk as ttk
+
+# GUI 視窗
+win = tkinter.Tk()
+win.title('攝氏華氏轉換') # 視窗標頭
+win.geometry("400x100")  # 視窗大小
+
+s = ttk.Style()
+s.configure('My.TButton', font=('Arial', 15))
+
+# 建立窗框
+my_frame_c = ttk.Frame(win)
+my_frame_f = ttk.Frame(win)
+my_frame_c.pack()
+my_frame_f.pack()
+
+# GUI 元件建立
+# 標籤 Label
+my_label_c = ttk.Label(my_frame_c, text='攝氏', font=('Arial', 15))
+my_label_f = ttk.Label(my_frame_f, text='華氏', font=('Arial', 15))
+# 輸入框
+my_entry_c = ttk.Entry(my_frame_c, font=('Arial', 15))
+my_entry_f = ttk.Entry(my_frame_f, font=('Arial', 15))
+# 按鈕 Button
+my_button_c = ttk.Button(my_frame_c, text='轉換', style='My.TButton')
+my_button_f = ttk.Button(my_frame_f, text='轉換', style='My.TButton')
+# 元件配置
+my_label_c.pack(side=tkinter.LEFT)
+my_entry_c.pack(side=tkinter.LEFT)
+my_button_c.pack(side=tkinter.LEFT)
+my_label_f.pack(side=tkinter.LEFT)
+my_entry_f.pack(side=tkinter.LEFT)
+my_button_f.pack(side=tkinter.LEFT)
+
+win.mainloop()
