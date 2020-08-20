a0397f416b01875209e6f0bb58c82a73f0849784
vincenttuan <vincenttuan@gmail.com>
2020-08-20 18:45:34 +0800

    Python

--- d15/MultiGUIComponents.py
+++ d15/MultiGUIComponents.py
@@ -9,7 +9,11 @@ win.geometry("300x300")  # 視窗大小
 # GUI 元件配置
 # 標籤 Label
 my_label = ttk.Label(win, text='我是標籤')
-my_label.pack()
+my_label.pack()  # 放置元件
+
+# 按鈕 Button
+my_button = ttk.Button(win, text='我是按鈕')
+my_button.pack()
 
 
 win.mainloop() # 視窗運行
