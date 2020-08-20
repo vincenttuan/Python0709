585bed54635517bca105b1f48dbd85a75c07cd4a
vincenttuan <vincenttuan@gmail.com>
2020-08-20 18:54:09 +0800

    Python

--- d15/MultiGUIComponents.py
+++ d15/MultiGUIComponents.py
@@ -15,6 +15,24 @@ my_label.pack()  # 放置元件
 my_button = ttk.Button(win, text='我是按鈕')
 my_button.pack()
 
+# 按鈕 CheckButton
+my_checkbutton = ttk.Checkbutton(win, text='同意')
+my_checkbutton.pack()
+
+# 按鈕 RadioButton
+m_radiobutton = ttk.Radiobutton(win, text="男", value=1)
+f_radiobutton = ttk.Radiobutton(win, text="女", value=2)
+m_radiobutton.pack()
+f_radiobutton.pack()
+
+# 輸入框
+my_entry = ttk.Entry(win)
+my_entry.pack()
+
+# 下拉選單
+values = ['Python', 'Java', 'VB']
+my_combobox = ttk.Combobox(win, value=values)
+my_combobox.pack()
 
 win.mainloop() # 視窗運行
 
