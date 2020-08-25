463741736c27b020a3b9faa30d7c7c98cc50143f
vincenttuan <vincenttuan@gmail.com>
2020-08-25 20:01:14 +0800

    Python

--- d16/SelectRecord.py
+++ d16/SelectRecord.py
@@ -3,6 +3,16 @@ import sqlite3
 conn = sqlite3.connect('demo.db')
 cursor = conn.cursor()
 
+# 表格欄位名稱(Table Meta-info)
+cursor.execute('PRAGMA TABLE_INFO({})'.format('lotto'))
+metainfo = cursor.fetchall()
+print('metainfo:', metainfo)
+names = [t[1] for t in metainfo] # id, n1, n3 ....
+print('names:', names)
+for name in names:
+    print(name, end='\t')
+
+print('\n-----------------------------------------------')
 # 查詢 SQL
 sql = 'SELECT id, n1, n2, n3, n4, n5, n6, ts FROM lotto'
 
