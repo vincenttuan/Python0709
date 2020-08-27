a6b679cfdb3ce83b8ac6dcd69cfa9202ff9e9496
vincenttuan <vincenttuan@gmail.com>
2020-08-27 21:21:32 +0800

    Python

--- d18/TreeViewStudent2.py
+++ d18/TreeViewStudent2.py
@@ -5,7 +5,17 @@ from tkinter import ttk
 conn = sqlite3.connect('student.db')
 cursor = conn.cursor()
 
+def add():
+    name = entry.get()
+    cursor.execute('INSERT INTO student(name) VALUES("%s")' % (name))
+    conn.commit()
+    query()
+
 def query():
+    # 清空 tree 的資料
+    for i in tree.get_children():
+        tree.delete(i)
+
     cursor.execute('select id, name from student')
     for row in cursor.fetchall():
         tree.insert('', 'end', values=row)
@@ -15,10 +25,10 @@ win = tkinter.Tk()
 entry = tkinter.Entry(win, justify=tkinter.CENTER)
 entry.pack()
 
-addButton = tkinter.Button(win, text='新增')
+addButton = tkinter.Button(win, text='新增', command=add)
 addButton.pack()
 
-queryButton = tkinter.Button(win, text='查詢')
+queryButton = tkinter.Button(win, text='查詢', command=query)
 queryButton.pack()
 
 tree = ttk.Treeview(win, columns=['1', '2'], show='headings') # 表格
